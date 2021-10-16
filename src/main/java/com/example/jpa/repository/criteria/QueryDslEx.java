09e23129f9c721acc23b5681d45f29bc68690ecb
KangChanSong <thdrkdcks@naver.com>
2021-10-16 20:35:28 +0900

    Tuple 을 이용한 프로젝션 조회

--- src/main/java/com/example/jpa/repository/criteria/QueryDslEx.java
+++ src/main/java/com/example/jpa/repository/criteria/QueryDslEx.java
@@ -3,6 +3,7 @@ package com.example.jpa.repository.criteria;
 import com.example.jpa.domain.Member;
 import com.example.jpa.domain.QMember;
 import com.example.jpa.main.Main;
+import com.mysema.query.Tuple;
 import com.mysema.query.jpa.JPASubQuery;
 import com.mysema.query.jpa.impl.JPAQuery;
 
@@ -13,13 +14,17 @@ import static com.example.jpa.domain.QMember.member;
 public class QueryDslEx {
     public static void main(String[] args) {
         Main.main(em -> {
-
+            JPAQuery query = new JPAQuery(em);
+            List<Tuple> tuples = query.from(member).list(member.username, member.age);
+            for (Tuple tuple: tuples) {
+                System.out.println("tuple.get(member.username) = " + tuple.get(member.username));
+                System.out.println("tuple.get(member.age) = " + tuple.get(member.age));
+            }
         });
     }
 
-    private void useSubQuery(EntityManager em){
+    private void useSubQuery(JPAQuery query){
         QMember memberSub = new QMember("memberSub");
-        JPAQuery query = new JPAQuery(em);
         List<Member> members = query.from(member)
                 .where(member.in(
                         new JPASubQuery().from(memberSub)
