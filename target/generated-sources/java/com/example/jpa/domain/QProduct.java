a96c5174aa8a635431e11a6f6e8993060021a19b
KangChanSong <thdrkdcks@naver.com>
2021-10-16 20:12:25 +0900

    Querydsl 쿼리 타입 생성

---
+++ target/generated-sources/java/com/example/jpa/domain/QProduct.java
@@ -0,0 +1,43 @@
+package com.example.jpa.domain;
+
+import static com.mysema.query.types.PathMetadataFactory.*;
+
+import com.mysema.query.types.path.*;
+
+import com.mysema.query.types.PathMetadata;
+import javax.annotation.Generated;
+import com.mysema.query.types.Path;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.mysema.query.codegen.EntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = -813732951L;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final NumberPath<Integer> price = createNumber("price", Integer.class);
+
+    public final NumberPath<Integer> stockAmount = createNumber("stockAmount", Integer.class);
+
+    public QProduct(String variable) {
+        super(Product.class, forVariable(variable));
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QProduct(PathMetadata<?> metadata) {
+        super(Product.class, metadata);
+    }
+
+}
+
