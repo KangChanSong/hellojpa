08ad6f5c7bd212e6ccdcd07997d385e52b8c1312
KangChanSong <thdrkdcks@naver.com>
2021-10-18 16:11:07 +0900

    주문 비즈니스 로직 테스트

---
+++ src/main/java/com/example/jpashop/service/OrderService.java
@@ -0,0 +1,53 @@
+package com.example.jpashop.service;
+
+import com.example.jpashop.domain.*;
+import com.example.jpashop.domain.item.Item;
+import com.example.jpashop.repository.MemberRepository;
+import com.example.jpashop.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class OrderService {
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    OrderRepository orderRepository;
+    @Autowired
+    ItemService itemService;
+
+    public Long order(Long memberId, Long itemId, int count){
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemService.findOne(itemId);
+
+        //배송 정보 생성
+        Delivery delivery = new Delivery(member.getAddress());
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    public void cancelOrder(Long orderId){
+
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();
+    }
+
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAll(orderSearch);
+    }
+}
