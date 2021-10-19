5ff579719a7f7d1915f765d59794a11fa6655d32
KangChanSong <thdrkdcks@naver.com>
2021-10-19 09:06:56 +0900

    프록시와 원본 동일성 테스트

--- src/test/java/com/example/jpashop/etc/EntityManagerLifeCycleTest.java
+++ src/test/java/com/example/jpashop/etc/EntityManagerLifeCycleTest.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
@@ -23,8 +26,11 @@ public class EntityManagerLifeCycleTest {
     @Autowired
     MemberService memberService;
 
+    @Autowired
+    EntityManager em;
+
     @Test
-    public void 동일성_테스트(){
+    public void 엔티티매니저_생명주기_동일성_테스트(){
         //given
         Member member = new Member();
         member.setName("asd");
@@ -34,6 +40,25 @@ public class EntityManagerLifeCycleTest {
         Member m2 = memberRepository.findOne(member.getId());
         //then
         assertNotEquals(m1, m2);
+    }
+
+    @Transactional
+    @Test
+    public void 프록시_원본_등일성_테스트(){
+        //given
+        Member member = new Member();
+        member.setName("asd");
+        memberRepository.save(member);
+        em.flush();
+        em.clear();
+
+        //when
+        Member ref = em.getReference(Member.class, member.getId());
+        Member member1 = em.find(Member.class, member.getId());
+
+        //then
+        assertEquals(ref, member1);
+        System.out.println("member1.getClass().getSimpleName() = " + member1.getClass().getSimpleName());
     }
 
 }
